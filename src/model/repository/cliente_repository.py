86eaebcd73d6a58071db67f364d77e1b3b3ead58
Marcio Silva <m.s.copr@gmail.com>
2024-09-15 16:40:23 -0300

    :sparkles: Modificado a classes ClienteRepository - trocado o argunto cpf do construtor pelo objeto cliente.
    Modificado o método consultar: ajustado a comparação do cpf para atender as modificações da classe.

--- src/model/repository/cliente_repository.py
+++ src/model/repository/cliente_repository.py
@@ -1,11 +1,11 @@
 
 class ClienteRepository:
-    def __init__(self, cpf, clientes):
-        self.cpf = cpf
+    def __init__(self, cliente, clientes):
+        self.cliente = cliente
         self.clientes = clientes
 
     def consultar(self):
-        clientes_filtrados = [cliente for cliente in self.clientes if cliente.cpf == self.cpf]
+        clientes_filtrados = [cliente for cliente in self.clientes if cliente.cpf == self.cliente.cpf]
 
         return clientes_filtrados[0] if clientes_filtrados else None
     

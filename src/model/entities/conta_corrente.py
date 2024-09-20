9b716a2477edecd9a46859631eb7470a89cdf262
Marcio Silva <m.s.copr@gmail.com>
2024-09-20 08:57:21 -0300

    :sparkles: Modificado a classe ContaCorrente: adicionado ao construtor o argunto sequencia_extrato.

--- src/model/entities/conta_corrente.py
+++ src/model/entities/conta_corrente.py
@@ -2,10 +2,12 @@
 from model.entities.movimentacoes import Movimentacoes
 
 class ContaCorrente:
-    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3, sequencia_extrato=0):
         self.numero = numero
-        self._cliente = cliente
-        self._limite = limite
-        self._limite_saques = limite_saques
-        self._saldo = 0
+        self.cliente = cliente
+        self.limite = limite
+        self.limite_saques = limite_saques
+        self.sequencia_extrato = sequencia_extrato
+        self.saldo = 0
         self.movimentacoes = Movimentacoes()
+    

071e0e626aa8e42edfbdfd5345ed4f311a317e16
Marcio Silva <m.s.copr@gmail.com>
2024-09-20 15:36:51 -0300

    :sparkles: Modificado a classe ContaCorrenteRepository: foram adionado dois métodos na classe, depositar e sacar, responsável por realizar credito e deito na conta corrente.

--- src/model/repository/conta_corrente_repository.py
+++ src/model/repository/conta_corrente_repository.py
@@ -1,5 +1,4 @@
 
-
 class ContaCorrenteRepository: 
     def __init__(self, conta_corrente, contas):
         self.conta_corrente = conta_corrente
@@ -10,3 +9,22 @@ class ContaCorrenteRepository:
         self.contas.append(conta_corrente)
 
         return len(self.contas) > 0
+
+    def depositar(self, conta, transacao):
+        saldo = conta.saldo
+
+        conta.saldo += transacao.valor
+            
+        return saldo < conta.saldo
+
+
+    def sacar(self, conta, transacao):
+        saldo = conta.saldo
+
+        conta.saldo -= transacao.valor
+        
+        return saldo > conta.saldo
+            
+            
+    def conta(self):
+        return self.contas[0]

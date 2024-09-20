f0e285e745d9dddc8d1c6f4450ecc5ac2c0a5153
Marcio Silva <m.s.copr@gmail.com>
2024-09-20 08:18:56 -0300

    :sparkles: Criado classes Format reponsável por da o formato de moeda a valores monetários.

---
+++ src/utility/format.py
@@ -0,0 +1,9 @@
+
+class Format:
+    def __init__(self, simbolo_moeda="R$"):
+        self.simbolo_moeda = simbolo_moeda
+
+    def moeda(self, valor):
+        return f"R$ {valor:,.2f}".replace(',', 'v').replace('.', ',').replace('v', '.') 
+        #return valor
+ 

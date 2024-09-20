afe48ed256550aab64892218f172c60fe94641ff
Marcio Silva <m.s.copr@gmail.com>
2024-09-20 09:03:53 -0300

    :sparkles: Modificado a classe Movimentacoes: Criado o método "lancar_transacao" responsável por adicionar a transa a lista de transações.
    Modificado o método efeturar_transacao: foi adionado ao mesmo a funcionalidade de lançar o saldo anterior(zerado) antes de lançar a primeira transação na conta corrente, caso a mesma não tenha nenhuma movimentação.

--- src/model/entities/movimentacoes.py
+++ src/model/entities/movimentacoes.py
@@ -1,16 +1,36 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import uuid
 
 class Movimentacoes:
     def __init__(self):
         self.transacoes = []
 
-    def efeturar_transacao(self, transacao):
+    def lancar_transacao(self, tid, data, tipo, valor, saldo):
         self.transacoes.append(
-            {
-                "tid:": uuid.uuid4(),
-                "tipo": transacao.__class__.__name__,
-                "valor": transacao.valor,
-                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            {           
+                "tid": tid,
+                "data": data,
+                "tipo": tipo,
+                "valor": valor,
+                "saldo": saldo,
             }
         )
+
+    def efeturar_transacao(self, conta_corrente, transacao):
+        if len(self.transacoes) == 0:
+            data = datetime.now()
+            data_anterior = data - timedelta(days=1)
+            
+            self.lancar_transacao("-------- Sem movimetações ---------", 
+                                  data_anterior.strftime("%d/%m/%Y %H:%M:%S"),
+                                  " -------- ",
+                                  0.00,
+                                  0.00
+                                  )
+        
+        self.lancar_transacao(uuid.uuid4(),
+                              datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                              transacao.__class__.__name__,
+                              transacao.valor,
+                              conta_corrente.saldo
+                              )

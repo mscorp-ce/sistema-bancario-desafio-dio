e352a67c49b5ec3f8e594e6dd7e604de48dfc223
Marcio Silva <m.s.copr@gmail.com>
2024-09-21 14:30:36 -0300

    :sparkles: Modificado a classe ContaCorrenteRepository
    Foi ajuste de espacamento entre os métodos.

--- src/model/repository/conta_corrente_repository.py
+++ src/model/repository/conta_corrente_repository.py
@@ -4,12 +4,14 @@ class ContaCorrenteRepository:
         self.conta_corrente = conta_corrente
         self.contas = contas
 
+
     def garvar(self, conta_corrente):
 
         self.contas.append(conta_corrente)
 
         return len(self.contas) > 0
 
+    
     def depositar(self, conta, transacao):
         saldo = conta.saldo
 

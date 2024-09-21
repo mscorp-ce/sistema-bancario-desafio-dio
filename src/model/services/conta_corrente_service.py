e973c6e05a251e1704dacdfffb78f039181af45b
Marcio Silva <m.s.copr@gmail.com>
2024-09-21 14:37:08 -0300

    :sparkles: Modificado a classe ContaCorrenteService
     - Criado o método validar_transacoes, responsável por filtrar todas as transações do dia corrente e verificar se o número máximo foi atingido .
    
     - Adaptados os métodos: depositar e sacar para atender a nova regra de negócio que estipula 10 transações por dia.

--- src/model/services/conta_corrente_service.py
+++ src/model/services/conta_corrente_service.py
@@ -11,9 +11,25 @@ class ContaCorrenteService:
         return self.repository.garvar(conta_corrente)
         
 
+    def validar_transacoes(self, conta): 
+        numero_transacoes_no_dia = len(conta.movimentacoes.filtrar_transacoes_por_dia())
+
+        if  numero_transacoes_no_dia == 10:
+            print("\nO número máximo de transações permitidas no dia foi excedido.")
+            return
+        
+        return numero_transacoes_no_dia
+
+
     def depositar(self, conta, transacao):
         if transacao.valor > 0:
-            return self.repository.depositar(conta, transacao)
+           
+           retorno = self.validar_transacoes(conta)
+
+           if retorno is None:
+               return
+
+           return self.repository.depositar(conta, transacao)
         else:
             print("\nO valor informado é inválido. Informe um valor maior do que zero.")
             return False
@@ -21,24 +37,16 @@ class ContaCorrenteService:
 
     def sacar(self, conta, transacao):
         if transacao.valor > 0:
-            numero_saques = sum(1 for transacao in conta.movimentacoes.transacoes if transacao['tipo'] == "Saque")
+            retorno = self.validar_transacoes(conta)
+
+            if retorno is None:
+                return
 
             if transacao.valor > conta.saldo:
                 print("\nSaldo insuficiente.")
                 return False
                 
-            limite_excedido = transacao.valor > conta.limite
-            
-            saques_excedido = numero_saques >= conta.limite_saques
-
-            if limite_excedido:
-                print("\nO valor do saque excede o seu limite.")
-                return False
-            elif saques_excedido:
-                print("\nO número máximo de saques foi excedido.")
-                return False
-
-            return self.repository.sacar(conta, transacao) # efetuar(conta, transacao)
+            return self.repository.sacar(conta, transacao)
         else:
             print("\nO valor informado é inválido. Inform um valor maior do que zero.")
             return False
